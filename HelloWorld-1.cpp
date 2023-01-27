ba4d9f9b27b39a4638e6c4e94bcbdab5d079b25e
Faizan Bhatti <FaizanaliB2@gmail.com>
2023-01-27 14:31:22 -0500

    First commit for Assignment 1

---
+++ HelloWorld-1.cpp
@@ -0,0 +1,16 @@
+// Simple C++ program to display "Hello World" 
+// Author Faizan Bhatti <gw8056@wayne.edu>
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World!"; 
+      
+    return 0; 
+} 
